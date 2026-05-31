799376e88681b5e073c3fabb4a98448fda87f2c0
cash-line <a.atris2022@gmail.com>
2026-05-31 12:01:20 +0300

    Static export for Cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
-import { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+const nextConfig = {
+  output: 'export' as const,
+  images: { unoptimized: true },
+};
 
-const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
-
-const nextConfig: NextConfig = {};
-
-export default withNextIntl(nextConfig);
+export default nextConfig;
